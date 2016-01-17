0b2f1dba5202d6693a8b16a93a6b7b1953ae68f4
shahriar99 <shahriar.jalili6666@yahoo.com>
2016-01-17 14:44:53 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {106953181},--Sudo users
+    sudo_users = {106953181,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
